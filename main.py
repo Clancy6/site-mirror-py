2012c45ee04ad6ec736bdf46de23a3f4fdb16edc
Clancy6 <118901657+Clancy6@users.noreply.github.com>
2024-08-14 18:35:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ import logging
 from crawler import Crawler
 from crawler.config import default_config
 
+import os, sys
+
 if __name__ == '__main__':
     config = {
         ## 目标网站主页
-        'main_url': 'https://www.lewenxiaoshuo.com/',
+        'main_url': sys.argv[1],
         ## 抓取深度, 从当前页面算起, 1表示只抓当前页, 0表示无限制
-        'max_depth': 1,
+        'max_depth': sys.argv[2],
         'headers': {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
         },
